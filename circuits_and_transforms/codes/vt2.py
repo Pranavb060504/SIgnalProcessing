334f2fa19c2bd8bf38aa76370a4bd4b7442efb5e
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-10-01 12:13:43 +0530

    Add files via upload

---
+++ circuits_and_transforms/codes/vt2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy
+def u(n):
+    if n<0:
+        return 0.0
+    if n>0:
+        return 1.0
+    if n==0:
+        return 0.5
+def volta(t):
+    return 2/3 *(1+np.exp(-1.5*t*(10**6)))*u(t)
+x=np.linspace(-10*10**(-6),10*10**(-6),100)
+vec_v=scipy.vectorize(volta)
+plt.plot(x,vec_v(x))
+plt.xlabel("time")
+plt.ylabel("$v_{C_0}(t)$")
+plt.grid()
+plt.show()
